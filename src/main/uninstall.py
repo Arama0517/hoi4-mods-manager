19c692410f1e09c931a41673fe150cfab96adfb7
Arama <arama.dev@outlook.com>
2024-10-07 03:55:35 +0800

    fix: 修复卸载不能正常删除模组的问题
    ai补全惹的祸...

--- src/main/uninstall.py
+++ src/main/uninstall.py
@@ -1,9 +1,16 @@
 import json
 import shutil
 
-from prompt_toolkit.shortcuts import message_dialog, radiolist_dialog
+from prompt_toolkit.shortcuts import checkboxlist_dialog, message_dialog
 
-from utils import LAUNCHER_SETTINGS, LAUNCHER_SETTINGS_FILE_PATH, MOD_BOOT_FILES_PATH, TITLE, clear
+from utils import (
+    LAUNCHER_SETTINGS,
+    LAUNCHER_SETTINGS_FILE_PATH,
+    MOD_BOOT_FILES_PATH,
+    MODS_DIR_PATH,
+    TITLE,
+    clear,
+)
 
 
 def main():
@@ -14,11 +21,12 @@ def main():
     if not options:
         message_dialog(TITLE, '你还没有安装任何模组', '返回').run()
         return
-    items_id = radiolist_dialog(TITLE, '请选择要卸载的模组', '卸载', '取消', options).run()
+    items_id = checkboxlist_dialog(TITLE, '请选择要卸载的模组', '卸载', '取消', options).run()
+    print(items_id)
     if not items_id:
         return
     for item_id in items_id:
-        shutil.rmtree(LAUNCHER_SETTINGS['mods'][item_id]['path'])
+        shutil.rmtree(MODS_DIR_PATH / item_id)
         del LAUNCHER_SETTINGS['mods'][item_id]
         (MOD_BOOT_FILES_PATH / f'{item_id}.mod').unlink(missing_ok=True)
     with LAUNCHER_SETTINGS_FILE_PATH.open('w', encoding='utf-8') as f:
